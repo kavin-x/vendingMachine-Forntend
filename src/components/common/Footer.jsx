6b0e035000c1bb8a5cab08ae3378ccb4df61034d
kavin-x <kavinkumarab@gmail.com>
2023-04-11 11:35:31 +0530

    inital commit

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,21 @@
+import * as Route from '@/constants/routes';
+import logo from '@/images/logo-full.png';
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+const Footer = () => {
+  const { pathname } = useLocation();
+
+  const visibleOnlyPath = [
+    Route.HOME,
+    Route.SHOP
+  ];
+
+  return !visibleOnlyPath.includes(pathname) ? null : (
+    <footer className="footer">
+      
+    </footer>
+  );
+};
+
+export default Footer;
